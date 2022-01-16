4b615815cd0e68d0cfd3af1d69eff9da7272fae8
Alex Eagleson <alexeagleson@gmail.com>
2022-01-15 23:46:59 -0500

    add emojis!! :fire::fire::penguin::boom:

--- src/camp-bot.ts
+++ src/camp-bot.ts
@@ -4,6 +4,15 @@ import { CronJob } from "cron";
 import { getDepartureDate, getPhrases } from "./notion";
 import { getDaysBetween, pickRandom } from "./utility";
 
+const emojis = [
+  [`:bullettrain_front:`, `:cloud:`],
+  [`:camping:`, `:coffee:`],
+  [`:potable_water:`, `:radio:`],
+  [`:chipmunk:`, `:hiking_boot:`],
+  [`:canned_food:`, `:hotdog:`],
+  [`:beer:`, `:beverage_box:`],
+] as const;
+
 const getMessage = async (): Promise<string> => {
   const phrases = await getPhrases();
   const currentDate = new Date();
@@ -20,7 +29,9 @@ const getMessage = async (): Promise<string> => {
     return "Check the random phrases in Notion, I couldn't find any";
   }
 
-  return `Only ${daysUntil} more days until ${randomPhrase}`;
+  const [emoji1, emoji2] = pickRandom(emojis)!;
+
+  return `${emoji1}  Only ${daysUntil} more days until ${randomPhrase}  ${emoji2}`;
 };
 
 const sendMessage = async () => {
@@ -42,7 +53,7 @@ const sendMessage = async () => {
       },
     },
     (res) => {
-      console.log(`statusCode: ${res.statusCode}`);
+      process.stdout.write(`statusCode: ${res.statusCode}\n`);
 
       res.on("data", (d) => {
         process.stdout.write(d);
